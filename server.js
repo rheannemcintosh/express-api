9d9a3919c9d3bd48721ded975d3c053c62a491c7
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-03-05 22:12:51 +0000

    Create a friends controller

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const friendsController = require('./controllers/friends.controller');
 const app = express();
 
 const PORT = 3000;
@@ -13,42 +14,13 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.post('/friends', (req, res) => {
-    if (!req.body.name) {
-        return res.status(400).json({
-            error: 'Missing friend name'
-        });
-    }
-
-    const newFriend = {
-        name: req.body.name,
-        id: friends.length
-    };
-
-    friends.push(newFriend);
-    res.json(newFriend);
-});
-
-app.get('/friends', (req, res) => {
-    res.json(friends);
-});
-
-app.get('/friends/:friendId', (req, res) => {
-    const friendId = Number(req.params.friendId);
-    const friend = friends[friendId];
-    if (friend) {
-        res.json(friend);
-    } else {
-        res.status(404).json({
-            error: "Friend does not exist"
-        });
-    }
-
-});
-
 app.get('/messages', (req, res) => {
     res.send('<ul><li>Hello!</li></ul>');
 })
+// Friends
+app.post('/friends', friendsController.postFriend);
+app.get('/friends', friendsController.getFriends);
+app.get('/friends/:friendId', friendsController.getFriend);
 
 app.post('/messages', (req, res) => {
     console.log('Updating Messages....');
